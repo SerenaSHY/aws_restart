1c9b86c7118c6441f69b6ae1fc988a1920a40177
SerenaSHY <64894116+SerenaSHY@users.noreply.github.com>
2026-04-13 20:40:53 +0800

    Delete reStart-python-cloud9/environment directory

--- reStart-python-cloud9/environment/analyze-insulin.py
+++
@@ -1,4 +0,0 @@
-with open("preproinsulin-seq-clean.txt", "r") as raw_file: 
-    cleaned_sequence = raw_file.read() 
-    cleaned_sequence_len = len(cleaned_sequence) 
-    print(f"The file has {cleaned_sequence_len}" characters)
